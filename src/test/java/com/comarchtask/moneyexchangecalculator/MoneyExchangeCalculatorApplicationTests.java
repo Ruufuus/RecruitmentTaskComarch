83987244bceff4246d7e12066b3972c7ae4bd7a3
Ruufuus <rufusikx3@gmail.com>
2022-08-24 10:13:05 +0200

    Initial commit

---
+++ src/test/java/com/comarchtask/moneyexchangecalculator/MoneyExchangeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comarchtask.moneyexchangecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyExchangeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
